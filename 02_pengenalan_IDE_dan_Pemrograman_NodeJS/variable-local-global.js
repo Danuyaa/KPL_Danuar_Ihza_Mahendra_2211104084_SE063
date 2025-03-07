59bd1c141c4c0cf38af9ca1013e5396ad3e604e5
Danuar Ihza Mahendra <danuarim09@gmail.com>
2025-03-07 20:17:45 +0700

    first commit

---
+++ 02_pengenalan_IDE_dan_Pemrograman_NodeJS/variable-local-global.js
@@ -0,0 +1,12 @@
+let globalVarible = "Danuar Ihza Mahendra"
+
+function getMyIdentity() {
+    let localVariable = 2211104084
+
+    console.log(`Nama saya ${globalVarible} nim ${localVariable}`)
+}
+
+console.log(globalVarible)
+// console.log(localVariable)
+
+getMyIdentity()
---
+++ 02_pengenalan_IDE_dan_Pemrograman_NodeJS/variable.js
@@ -0,0 +1,17 @@
+var name = 'Danuar Ihza Mahendra'
+const nim = 2211104084
+
+console.log(name)
+console.log(nim)
+
+console.log('nama saya adalah ' + name + ' dengan NIM ' + nim)
+console.log(`nama saya adalah ${name} dengan NIM ${nim}`)
+
+
+console.log(`Nama saya adalah ${name}
+    nim saya adalah ${nim}`)
+
+
+var name = "ayam"
+
+let fullName = "Danuar Ihza Mahendra"
